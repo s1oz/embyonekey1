275d494740bc59b66340808d307c06b88366f0ef
s1oz <1412.kidd0@gmail.com>
2021-07-28 14:00:41 +0800

    fix

---
+++ embyonekey.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd /var/packages/EmbyServer/target/mono/bin
+curl https://raw.githubusercontent.com/s1oz/embyonekey1/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+./cert-sync /etc/ssl/certs/ca-certificates.crt
+cd /usr/local/etc/nginx/conf.d/*-*-*-*
+wget https://raw.githubusercontent.com/s1oz/embyonekey1/master/user.conf
+nginx -s reload
+
